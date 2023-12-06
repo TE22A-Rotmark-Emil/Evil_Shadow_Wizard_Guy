e17cc94132989369a666e4399692541ca371d151
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2023-12-06 09:25:55 +0100

    Finalized Camera Script

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -9,7 +9,7 @@ public class CameraScript : MonoBehaviour
 
     Vector3 cameraOffset = new(0, 3, -7f);
 
-    float timeSmoothing = 0.25f;
+    float timeSmoothing = 0.75f;
 
     Vector3 velocity = Vector3.zero;
 
@@ -21,12 +21,10 @@ public class CameraScript : MonoBehaviour
 
     void LateUpdate(){
         playerPosition = player.position + cameraOffset;
-        Vector3 playerPosY = new(0, player.position.y, 0);
         
         //add offset +2 -2 so the camera is not focused on the player and only follows them if the player moves outside of +2 -2
-        if (playerPosition.x - cameraOffset.x > 2){
-            
+        if (playerPosition.x - transform.position.x > 2 || player.position.x - transform.position.x < -2){
+            transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, timeSmoothing);
         }
-        transform.position = Vector3.SmoothDamp(transform.position, playerPosition, ref velocity, timeSmoothing);
     }
 }
